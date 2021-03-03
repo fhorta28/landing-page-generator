1957fc9cd6ba2bdca70184ddd4fc282480a17367
sihem laffat <76011183+SihemLaffat@users.noreply.github.com>
2021-03-03 13:04:43 +0100

    update

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,16 +3,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 // Intern
 import { Container } from './style' 
+import ModaleFunction from '../Modale/ModaleFunction'
+import Modale from '../Modale/index'
 
 
 const Button = props => {
   // Props & States
   const { backgroundColor, textColor, content, onClick } = props
+  const {openModale, toggleModale} = ModaleFunction();
 
   return (
-    <Container backgroundColor={backgroundColor} textColor={textColor} onClick={onClick}>
-      {content}
-    </Container>
+    <>
+      <Container backgroundColor={backgroundColor} textColor={textColor} onClick={toggleModale}>
+        {content}
+      </Container>
+      <Modale></Modale>
+    </>
   )
 }
 
---
+++ src/components/Modale/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+import * as modaleForm from'../../variables/modalForm'
+
+export const StyleModale = styled.div`
+
+
+
+
+`
+
+
+
+
+
